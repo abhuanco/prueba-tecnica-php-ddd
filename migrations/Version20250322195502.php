25ee055c1275f33971bcec6b48b6b660054a623c
Rene Huanco Choque <rene.huanco.choque@gmail.com>
2025-03-22 17:08:52 -0400

    Add telephone column to users table and update email, password, name, and created_at fields

---
+++ migrations/Version20250322195502.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250322195502 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE users ADD telephone VARCHAR(12) NOT NULL, CHANGE email email VARCHAR(150) NOT NULL, CHANGE password password VARCHAR(150) NOT NULL, CHANGE name name VARCHAR(150) NOT NULL, CHANGE created_at created_at DATETIME NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_1483A5E9E7927C74 ON users');
+        $this->addSql('ALTER TABLE users DROP telephone, CHANGE name name VARCHAR(255) DEFAULT NULL, CHANGE email email VARCHAR(255) DEFAULT NULL, CHANGE password password VARCHAR(255) DEFAULT NULL, CHANGE created_at created_at DATE DEFAULT NULL');
+    }
+}
